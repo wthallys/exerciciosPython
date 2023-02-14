6c13093aa27103f4abfe933aedbc3b2e80bc8bc9
wthallys <wthallys.02@gmail.com>
2023-02-14 20:18:28 -0300

    updated fatorial

--- faculdade/fatorial.py
+++ faculdade/fatorial.py
@@ -1,6 +1,17 @@
-def fatorial(num):
-    if num == 1 or num == 0:
-        return 1
-    return num * fatorial(num - 1)
+# num = int(input("Digite um numero inteiro:"))
 
-print(fatorial(5))
+# def fatorial(num):
+#     if num == 1 or num == 0:
+#         return 1
+#     return num * fatorial(num - 1)
+
+# print(f'Fatorial: {fatorial(num)}')
+
+num = int(input("Digite um numero inteiro:"))
+
+fatorial = 1
+while (num > 1):
+    fatorial = fatorial * num
+    num -= 1
+print()
+print(f'Fatorial: {fatorial}')
